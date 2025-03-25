2f7a0b7f0a21d8f9ca0491ef702bb8498ccb96a7
dinakaranthiru <dinakarant09@gmail.com>
2025-03-25 15:24:56 +0530

    implemented the favorite option

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Favorite from "./pages/Favorite";
 import NavBar from "./components/NavBar";
+import { MovieProvider } from "./contexts/MovieContext";
 
 function App() {
   return (
-    <div>
-      <NavBar/>
-      <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/favorite" element={<Favorite/>} />
-
-      </Routes>
-    </main>
-
-    </div>
+    
+      <MovieProvider>
+        <NavBar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorite" element={<Favorite />} />
+          </Routes>
+        </main>
+      </MovieProvider>
     
   );
 }
